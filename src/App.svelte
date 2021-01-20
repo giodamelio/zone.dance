9d1aa3bf745ee3ae59069c4e64b46a0242d51a8a
Gio d'Amelio <giodamelio@gmail.com>
2021-01-20 12:46:40 -0600

    Setup prettier and format existing files

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-	import { count } from "./stores";
+  import { count } from './stores';
 
-	export let name: string = "HAHA";
+  export let name: string = 'HAHA';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+  <h1>Hello {name}!</h1>
 
-	<p>
-		There is nothing here right now, but you should check back every day to make
-		sure!
-	</p>
+  <p>
+    There is nothing here right now, but you should check back every day to make
+    sure!
+  </p>
 
-	<p>The current count is at: {$count}</p>
-	<button on:click={() => count.update((n) => n + 1)}>+</button>
-	<button on:click={() => count.update((n) => n - 1)}>-</button>
+  <p>The current count is at: {$count}</p>
+  <button on:click={() => count.update((n) => n + 1)}>+</button>
+  <button on:click={() => count.update((n) => n - 1)}>-</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
